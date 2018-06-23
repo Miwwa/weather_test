3e04f3eb3d3c0f33aaeaaa1fad7ae917196e5ffa
Michail <meremin92@mail.ru>
2018-06-23 08:44:30 +0300

    create weather store and weather api

---
+++ src/WeatherApi.js
@@ -0,0 +1,33 @@
+const ApiUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const ApiKey = '95900833c2fb71d9f4e8b3a8cc25e608';
+
+async function fetchWeather (url) {
+  const res  = await window.fetch(url);
+  const json = await res.json();
+
+  if (json.cod != 200)
+    throw new Error(json.message);
+
+  return {
+    id: json.id,
+    name: json.name,
+    weather: json.weather[0],
+    temp: json.main.temp,
+    wind: json.wind,
+  };
+}
+
+export default {
+  fetchByCityName: async (cityName) => {
+    const url = `${ApiUrl}?q=${cityName}&units=metric&APPID=${ApiKey}`;
+    return await fetchWeather(url);
+  },
+  fetchByCityId:   async (cityId) => {
+    const url = `${ApiUrl}?id=${cityId}&units=metric&APPID=${ApiKey}`;
+    return await fetchWeather(url);
+  },
+  fetchByCoords:   async (coords) => {
+    const url = `${ApiUrl}?lat=${coords.lat}&lon=${coords.lon}&units=metric&APPID=${ApiKey}`;
+    return await fetchWeather(url);
+  }
+};
---
+++ src/WeatherStore.js
@@ -0,0 +1,143 @@
+import {observable, action} from 'mobx';
+
+const WeatherCacheKey = 'weather';
+const ErrorsText      = {
+  AlreadyInList: 'City already in list',
+  NotString:     'City must be a string',
+  StringEmpty:   'Input are empty'
+};
+
+class WeatherStore {
+  @observable state = {
+    loading: false,
+    error:   null
+  };
+
+  @observable citiesList = [];
+
+  constructor (api) {
+    this.api        = api;
+    this.citiesList = WeatherStore._loadFromCache();
+  }
+
+  /**
+   * Add city by name
+   * @param cityName
+   * @returns {Promise<void>}
+   */
+  @action
+  async addCityByName (cityName) {
+    const error = WeatherStore._validateCityName(cityName);
+    if (error) {
+      this.state.error = error;
+      return;
+    }
+
+    if (this.citiesList.filter(city => city.name === cityName).length > 0) {
+      this.state.error = ErrorsText.AlreadyInList;
+      return;
+    }
+
+    this.state.loading = true;
+    this.state.error   = null;
+
+    try {
+      const result = await this.api.fetchByCityName(cityName);
+      if (this.citiesList.filter(city => city.id === result.id).length > 0) {
+        this.state.error = ErrorsText.AlreadyInList;
+      } else {
+        this.citiesList.push({...result, loading: false, updatedAt: Date.now()});
+        WeatherStore._saveToCache(this.citiesList);
+      }
+      this.state.loading = false;
+    } catch (e) {
+      this.state.error   = e.message;
+      this.state.loading = false;
+    }
+  }
+
+  /**
+   * Update city weather
+   * @param city
+   * @returns {Promise<void>}
+   */
+  @action
+  async updateCity (city) {
+    city.loading = true;
+    city.error   = null;
+    try {
+      const result = await this.api.fetchByCityId(city.id);
+      Object.assign(city, {
+        ...result,
+        updatedAt: Date.now(),
+        loading: false
+      });
+      WeatherStore._saveToCache(this.citiesList);
+    } catch (e) {
+      city.error   = e.message;
+      city.loading = false;
+    }
+  }
+
+  /**
+   * Remove city from list
+   * @param city
+   */
+  @action removeCity (city) {
+    this.citiesList.remove(city);
+    WeatherStore._saveToCache(this.citiesList);
+  }
+
+  /**
+   * Validate city name
+   * @param cityName
+   * @returns {string|null} null if valid, else string error
+   * @private
+   */
+  static _validateCityName (cityName) {
+    if (typeof cityName !== 'string')
+      return ErrorsText.NotString;
+    if (cityName.trim().length === 0)
+      return ErrorsText.StringEmpty;
+    return null;
+  }
+
+  /**
+   * Save current weather info to local storage
+   * @private
+   */
+  static _saveToCache (data) {
+    if (!window.localStorage)
+      return;
+
+    try {
+      const citiesJson = JSON.stringify(data);
+      window.localStorage.setItem(WeatherCacheKey, citiesJson);
+    } catch (e) {
+      console.log('Save to cache error', e);
+    }
+  }
+
+  /**
+   * Load weather info from local storage
+   * @returns {array}
+   * @private
+   */
+  static _loadFromCache () {
+    if (!window.localStorage)
+      return [];
+
+    try {
+      const cache = window.localStorage.getItem(WeatherCacheKey);
+      if (cache != null) {
+        return JSON.parse(cache);
+      }
+    } catch (e) {
+      console.warn('Can\'t parse cached weather', e);
+      return [];
+    }
+    return [];
+  }
+}
+
+export default WeatherStore;
